556815a195a6a012053f18faf2cdebe3a338556c
Rita Moura <ritamoura520@gmail.com>
2023-10-09 16:11:34 -0300

    Feat: adiciona a função Generate em TokenManager.

--- src/TryBets.Users/Services/TokenManager.cs
+++ src/TryBets.Users/Services/TokenManager.cs
@@ -20,7 +20,22 @@ namespace TryBets.Users.Services
 
         public string Generate(User user)
         {
-            throw new NotImplementedException();
+            var identity = new ClaimsIdentity();
+            identity.AddClaim(new Claim(ClaimTypes.Email, user.Email!));
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identity,
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_tokenOptions.Secret!)),
+                    SecurityAlgorithms.HmacSha256Signature
+                ),
+                Expires = DateTime.UtcNow.AddDays(_tokenOptions.ExpiresDay)
+            };
+
+            var generatedToken = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(generatedToken);
         }
     }
 }
