ddc159bb538d701abc7aa9d355f62bcb1c23c432
Rita Moura <ritamoura520@gmail.com>
2023-10-09 16:10:29 -0300

    Feat: adiciona Post e Login ao UserRepository.

--- src/TryBets.Users/Repository/UserRepository.cs
+++ src/TryBets.Users/Repository/UserRepository.cs
@@ -13,10 +13,29 @@ public class UserRepository : IUserRepository
 
     public User Post(User user)
     {
-        throw new NotImplementedException();
+        var verifyUser = _context.Users.Where(u => u.Email == user.Email);
+
+        if (verifyUser.Count() > 0)
+        {
+            throw new Exception("E-mail already used");
+        }
+
+        _context.Users.Add(user);
+        _context.SaveChanges();
+
+        return user;
     }
+
     public User Login(AuthDTORequest login)
     {
-       throw new NotImplementedException();
+        var user = _context.Users.Where(u => u.Email == login.Email);
+
+        if (user.Count() == 0 || user.First().Password != login.Password)
+        {
+            throw new Exception("Authentication failed");
+        }
+
+        return user.FirstOrDefault()!;
     }
+
 }
