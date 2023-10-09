40358ddd6812d438ed4effef7594b3f1c50cd41f
Rita Moura <ritamoura520@gmail.com>
2023-10-09 18:02:48 -0300

    Feat: adiciona UpdateOdd a OddService.

--- src/TryBets.Bets/Services/OddService.cs
+++ src/TryBets.Bets/Services/OddService.cs
@@ -1,4 +1,6 @@
 using System.Net.Http;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 namespace TryBets.Bets.Services;
 
 public class OddService : IOddService
@@ -11,6 +13,23 @@ public class OddService : IOddService
 
     public async Task<object> UpdateOdd(int MatchId, int TeamId, decimal BetValue)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var url = new UriBuilder("http://localhost:5504/odd")
+            {
+                Path = $"/{MatchId}/{TeamId}/{BetValue}"
+            };
+            var convert = url.Uri.ToString();
+            var request = new StringContent(string.Empty, System.Text.Encoding.UTF8, "application/json");
+            var response = await _client.PatchAsync(convert, request);
+            var content = await response.Content.ReadAsStringAsync();
+
+            return content;
+        }
+        catch (Exception ex)
+        {
+            return ex.Message;
+        }
+        
     }
 }
